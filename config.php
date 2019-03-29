31cec0024f4d801cf3bbd1d6fbb3864a9c1ad6fc
Mykola-Syniavskyi <44326435+Mykola-Syniavskyi@users.noreply.github.com>
2019-03-29 22:39:58 +0200

    remove files

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('LOCAL_HOST', '127.0.0.1');
-define('USER_15', 'root');
-define('PASSWD', '');
-define('DB_NAME', 'test');
-
-
-
-?>
--- mysql.php
+++
@@ -1,6 +0,0 @@
-<?php
-class MYSQL extends SQL
-{
-    
-}
-?>
